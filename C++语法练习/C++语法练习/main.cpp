bd054865b70513983ee5fb9fe415b0ecd8104868
黄来峰 <751362521@qq.com>
2017-03-30 23:56:23 +0800

    c++d的输入输出的调用

--- C++语法练习/C++语法练习/main.cpp
+++ C++语法练习/C++语法练习/main.cpp
@@ -7,9 +7,25 @@
 //
 
 #include <iostream>
+//导入相关的库
+#include <stdlib.h>
+//告诉系统使用某个命名空间的方法
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+//    C++中的输入输出展示
+    cout << "请输入一个整数:" << endl;
+    int x = 0;
+    cin >> x;
+    cout << dec << x << endl;
+    cout << oct << x << endl;
+    cout << hex << x << endl;
+    
+    cout << "请输入一个布尔值（0或则1）:" << endl;
+    bool y = false;
+    cin >> y;
+    cout << boolalpha << y;
+    
     return 0;
 }

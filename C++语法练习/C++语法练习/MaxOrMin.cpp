7eb35db35873f04f104cc95edbfe9772468b16de
黄来峰 <751362521@qq.com>
2017-03-31 22:15:05 +0800

    基本语法练习

---
+++ C++语法练习/C++语法练习/MaxOrMin.cpp
@@ -0,0 +1,9 @@
+//
+//  MaxOrMin.cpp
+//  C++语法练习
+//
+//  Created by 黄来峰 on 2017/3/31.
+//  Copyright © 2017年 com.gmw.love. All rights reserved.
+//
+
+#include "MaxOrMin.hpp"
--- C++语法练习/C++语法练习/main.cpp
+++ C++语法练习/C++语法练习/main.cpp
@@ -12,9 +12,21 @@
 //告诉系统使用某个命名空间的方法
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    
-//    C++中的输入输出展示
+namespace companyA {
+    int x = 10;
+    void function(){
+        cout << "companyA" << endl;
+    }
+}
+namespace companyB {
+    int y = 200;
+    void function(){
+        cout << "companyB" << endl;
+    }
+}
+
+void functionOfFirst(){
+    //    C++中的输入输出展示
     cout << "请输入一个整数:" << endl;
     int x = 0;
     cin >> x;
@@ -24,8 +36,20 @@ int main(int argc, const char * argv[]) {
     
     cout << "请输入一个布尔值（0或则1）:" << endl;
     bool y = false;
-    cin >> y;
+    cin >> y ;
     cout << boolalpha << y;
+    
+    
+}
+int main(int argc, const char * argv[]) {
+    
+//    cout << companyA::x << endl;
+//    companyB::function();
+    functionOfFirst();
+    
+    //命名空间的使用
     
     return 0;
 }
+
+

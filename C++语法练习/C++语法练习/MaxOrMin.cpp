0d73c775a129067a2f98c8b4c46d385320f57824
黄来峰 <751362521@qq.com>
2017-04-02 14:38:45 +0800

    命名空间的使用，给变量起别名，定义指针的引用、定义结构体的引用

--- C++语法练习/C++语法练习/MaxOrMin.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  MaxOrMin.cpp
-//  C++语法练习
-//
-//  Created by 黄来峰 on 2017/3/31.
-//  Copyright © 2017年 com.gmw.love. All rights reserved.
-//
-
-#include "MaxOrMin.hpp"
--- C++语法练习/C++语法练习/main.cpp
+++ C++语法练习/C++语法练习/main.cpp
@@ -9,6 +9,8 @@
 #include <iostream>
 //导入相关的库
 #include <stdlib.h>
+void pointerArg(int &a, int &b);
+
 //告诉系统使用某个命名空间的方法
 using namespace std;
 
@@ -24,6 +26,62 @@ namespace companyB {
         cout << "companyB" << endl;
     }
 }
+
+//起别名
+namespace companyOtherName {
+    void functionOtherName(){
+        //命名空间的使用
+        int a = 10;
+        int &b = a;
+        
+        b = 20;
+        cout << a << endl;
+        
+        a = 30;
+        
+        cout << b << endl;
+        
+
+    
+    }
+}
+
+//如何给一个结构体起别名
+namespace structOtherName {
+    
+    typedef struct {
+    
+        int x;
+        int y;
+    
+    }myStruct;
+    
+    void functionName(){
+        myStruct coord;
+        myStruct &structA = coord;
+        structA.x = 100;
+        structA.y = 20;
+        cout << coord.x << endl <<coord.y << endl;
+    }
+}
+
+//给指针使用引用
+namespace otherNameOfPointer {
+    
+    void pointer_Index(){
+    
+        int a = 3;
+        int *p = &a;
+        int *&q = p;
+        *q = 5;
+        cout << a << endl;
+        
+    };
+    
+    
+}
+
+
 
 void functionOfFirst(){
     //    C++中的输入输出展示
@@ -41,15 +99,30 @@ void functionOfFirst(){
     
     
 }
+
+void functionOfSecond(int &a, int &b);
 int main(int argc, const char * argv[]) {
     
 //    cout << companyA::x << endl;
 //    companyB::function();
-    functionOfFirst();
-    
-    //命名空间的使用
+//    functionOfFirst();
+//    companyOtherName::functionOtherName();
+//    structOtherName::functionName();
+//    otherNameOfPointer::pointer_Index();
+    int a = 200;
+    int b = 100;
+    functionOfSecond(a, b);
     
     return 0;
 }
 
-
+//给函数参数使用引用
+void functionOfSecond(int &a, int &b){
+    
+    int c = 0;
+    c = a;
+    a = b;
+    b = c;
+    
+    cout << a << endl << b <<endl ;
+}

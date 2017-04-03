36faa3d9e883fc6bb6c4fb454fa11c924e66fe3d
黄来峰 <751362521@qq.com>
2017-04-03 10:46:51 +0800

    创建项目，演示Const在C++中的使用

---
+++ 演示Const在函数中的使用/演示Const在函数中的使用/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  演示Const在函数中的使用
+//
+//  Created by 黄来峰 on 2017/4/3.
+//  Copyright © 2017年 com.gmw.love. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

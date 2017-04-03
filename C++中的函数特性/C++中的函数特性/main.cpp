0bfe9060eb785011e2d90bc5be2af6db23241e2c
黄来峰 <751362521@qq.com>
2017-04-03 14:04:00 +0800

    增加函数重载部分的示例

--- C++中的函数特性/C++中的函数特性/main.cpp
+++ C++中的函数特性/C++中的函数特性/main.cpp
@@ -12,15 +12,26 @@ using namespace std;
 
 //声明
 void funcTionOfDefaultValue(int x = 10,int y = 20,int z = 30);
+void funcTionOfDefaultValue(double x ,double y = 5.3, double z = 6.5 );
 int main(int argc, const char * argv[]) {
-    funcTionOfDefaultValue();
+//    --------------------函数默认值部分
+//    funcTionOfDefaultValue();
     funcTionOfDefaultValue(100);
     funcTionOfDefaultValue(150,200);
     funcTionOfDefaultValue(300,400,500);
+//    -----------------------
+    funcTionOfDefaultValue(1.2,1.3);
+//    ------------------函数重载部分
+   
+    
     
     return 0;
 }
 
+void funcTionOfDefaultValue(double x,double y, double z){
+    cout << x << "," << y << "," << z << endl ;
+
+}
 
 void funcTionOfDefaultValue(int i,int j,int k){
 

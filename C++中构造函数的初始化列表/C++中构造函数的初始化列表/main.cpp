831f8a5237a3ad7f9c8de624851272bddf63bd76
黄来峰 <751362521@qq.com>
2017-04-05 23:28:40 +0800

    C++之初始化列表讲解

---
+++ C++中构造函数的初始化列表/C++中构造函数的初始化列表/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++中构造函数的初始化列表
+//
+//  Created by 黄来峰 on 2017/4/5.
+//  Copyright © 2017年 com.gmw.love. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

b4c403db95eeae05fb24df067dda080f35866dd6
黄来峰 <751362521@qq.com>
2017-04-05 18:54:07 +0800

    同文件和不同文件的类外定义--工程创建

---
+++ 同文件和不同文件的类外定义/同文件和不同文件的类外定义/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  同文件和不同文件的类外定义
+//
+//  Created by 黄来峰 on 2017/4/5.
+//  Copyright © 2017年 com.gmw.love. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

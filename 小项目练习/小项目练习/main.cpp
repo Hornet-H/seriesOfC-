14fa8ae87b9feb88ee653e353a7a62da36d9da3b
黄来峰 <751362521@qq.com>
2017-04-05 16:15:41 +0800

    小项目完成

--- 小项目练习/小项目练习/main.cpp
+++ 小项目练习/小项目练习/main.cpp
@@ -4,12 +4,37 @@
 //
 //  Created by 黄来峰 on 2017/4/5.
 //  Copyright © 2017年 com.gmw.love. All rights reserved.
-//
+/*
+ 
+ 题目描述：
+ 1、提示用户输入姓名
+ 2、接受用户的输入
+ 3、然后像用问好
+ 4、告诉用户名字的长度
+ 5、告诉用户名字的首字母是什么
+ 6、如果用户直接输入回车，那么告诉用户的输入为空
+ 7、如果用户的输入为imooc，那么告诉用户额角色是一个管理员
+ 
+ */
 
 #include <iostream>
+#include <stdlib.h>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    string name;
+    cout << "请输入您的姓名" << endl;
+    getline(cin, name);
+    if (name.empty()) {
+        cout << "您输入的为空请重新输入" << endl;
+    }
+    if (name == "imooc") {
+        cout << "您是一个管理员" << endl;
+    }
+    cout << "hello" + name << endl;
+    
+    cout << "你名字的长度是:" << name.size() << endl;
+    cout << " 您名字的首字母是:" << name[0] <<endl;
     return 0;
 }
